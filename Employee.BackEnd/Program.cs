7d90921362efda611e05082a68ea261503d5a869
tanjim31 <rahatahmedtanjim1234@gmail.com>
2023-09-30 16:55:05 +0600

    ReDoc Added

--- Employee.BackEnd/Program.cs
+++ Employee.BackEnd/Program.cs
@@ -1,4 +1,7 @@
 using Employee.IoC.Configuration;
+using Microsoft.OpenApi.Models;
+using System.ComponentModel.DataAnnotations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,25 @@ builder.Services.AddControllers();
 builder.Services.MapCore(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//add redoc
+builder.Services.AddSwaggerGen(options =>
+{
+options.SwaggerDoc("v1",
+    new OpenApiInfo
+    {
+        Title = "Employee",
+        Version = "v1",
+        Description = "This is a Employee Project to see how documentation can easily for generated for ASP.NET Core Web APIs using Swagger and ReDoc",
+        Contact = new OpenApiContact
+        {
+            Name="Rahat Ahmed Tanjim",
+            Email="rahatahmedtanjim1234@gmail.com"
+        }
+    });
+        
+});
+//end redoc
 
 var app = builder.Build();
 
@@ -15,7 +36,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+
+    //add it
+    app.UseSwaggerUI(options =>
+    options.SwaggerEndpoint("/swagger/v1/swagger.json","Demo Documentation v1"));
+    app.UseReDoc(options =>
+    {
+        options.DocumentTitle = "Demo Documentaion";
+        options.SpecUrl = "/swagger/v1/swagger.json";
+    });
+    //
 }
 
 app.UseHttpsRedirection();
--- Frontend/Employee.Frontend/Controllers/EmployeeController.cs
+++ Frontend/Employee.Frontend/Controllers/EmployeeController.cs
@@ -1,7 +1,9 @@
 ﻿using Employee.Frontend.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using System.Text.Json;
+
 
 namespace Employee.Frontend.Controllers;
 
@@ -16,6 +18,45 @@ public class EmployeeController : Controller
     {
         var response = await _httpClient.GetFromJsonAsync<List<Employeest>>("Employee");
         return response is not null? response : new List<Employeest>();
+    }
+
+    public async Task<IActionResult> AddOrEdit(int Id)
+    {
+        var response= await _httpClient.GetAsync("Country");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var countryList= JsonConvert.DeserializeObject<List<Country>>(content);
+            ViewData["countryId"] = new SelectList(countryList, "Id", "CountryName");
+        }
+        var response2 = await _httpClient.GetAsync("State");
+        if (response.IsSuccessStatusCode)
+        {
+            var content= await response2.Content.ReadAsStringAsync();
+            var countryList= JsonConvert.DeserializeObject<List<State>>(content);
+            ViewData["stateId"] = new SelectList(countryList, "Id", "StateName");
+
+        }
+
+
+        if (Id == 0)
+        {
+            //Create Form
+            return View(new Employeest());
+        }
+        else
+        {
+            //Get By Id
+            var data = await _httpClient.GetAsync($"Country/{Id}");
+            if (data.IsSuccessStatusCode)
+            {
+                var result = await data.Content.ReadFromJsonAsync<Employeest>();
+                return View(result);
+            }
+        }
+        return View(new Country());
+
+
     }
 
     
